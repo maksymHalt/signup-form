39b33e2c4a2054d27327a13f850e12f6cada07dd
Maksym Haltsev <maksym.haltsev@gmail.com>
2019-04-13 15:58:45 +0300

    remove useless tests

--- src/components/SelectField/SelectField.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import SelectField from './SelectField'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<SelectField />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
--- src/containers/BaseInfoStep/BaseInfoStep.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import BaseInfoStep from './BaseInfoStep'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<BaseInfoStep />, div)
-  ReactDOM.unmountComponentAtNode(div)
-})
